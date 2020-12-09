6ba1443672cf4233736f9748fabe5bbd3a147e99
kaliss96 <aguirretellez96@gmail.com>
2020-12-09 16:07:39 -0600

    actualiacion de login FACE

--- ConexionApiRestAndroidProject/app/src/main/java/com/example/logindemo/MainActivity.kt
+++ ConexionApiRestAndroidProject/app/src/main/java/com/example/logindemo/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.logindemo
 
-import android.app.PendingIntent.getActivity
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.facebook.*
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -14,9 +19,15 @@ import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 const val RC_SIGN_IN=123
 
 class MainActivity : AppCompatActivity() {
+    private val EMAIL = "email"
+
+    private val TAG = "FacebookLogin"
+    private var mCallbackManager: CallbackManager? = null
+    lateinit var signOut: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,8 +35,61 @@ class MainActivity : AppCompatActivity() {
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
 
+        // Initialize Facebook Login button
+        mCallbackManager = CallbackManager.Factory.create()
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+
+        val loginButton = findViewById<LoginButton>(R.id.login_fbutton)
+        /*val accessToken = AccessToken.getCurrentAccessToken()
+        val isLoggedIn = accessToken != null && !accessToken.isExpired*/
+
+        //LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+        LoginManager.getInstance().retrieveLoginStatus(this, object : LoginStatusCallback {
+            override fun onCompleted(accessToken: AccessToken) {
+                // User was previously logged in, can log them in directly here.
+                // If this callback is called, a popup notification appears that says
+                // "Logged in as <User Name>"
+
+            }
+
+            override fun onFailure() {
+                // No access token could be retrieved for the user
+            }
+
+            override fun onError(exception: Exception) {
+                // An error occurred
+            }
+        })
+
+        loginButton.setReadPermissions("email", "public_profile")
+        loginButton.registerCallback(mCallbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                Log.d(TAG, "facebook:onSuccess:$loginResult")
+
+              //
+               /* Toast.makeText(applicationContext, loginResult.getAccessToken().getToken(), Toast.LENGTH_LONG)
+                    .show()*/
+
+
+                val intent = Intent(application, Participantes::class.java)
+                intent.putExtra("tokefb", loginResult.getAccessToken().getToken())
+                startActivity(intent)
+                finish();
+
+            }
+
+            override fun onCancel() {
+                Log.d(TAG, "facebook:onCancel")
+            }
+
+            override fun onError(error: FacebookException) {}
+        })
+
+
+
+
+
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.client_googleid))
                 .requestEmail()
@@ -33,9 +97,9 @@ class MainActivity : AppCompatActivity() {
 
         // Build a GoogleSignInClient with the options specified by gso.
         val mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-
+       // signOut = findViewById<View>(R.id.sign_out_button) as Button
         sign_in_button.visibility = View.VISIBLE
-        tv_name.visibility = View.GONE
+        result_name.visibility = View.GONE
 
         sign_in_button.setSize(SignInButton.SIZE_STANDARD);
         sign_in_button.setOnClickListener{
@@ -45,12 +109,30 @@ class MainActivity : AppCompatActivity() {
         val acct = GoogleSignIn.getLastSignedInAccount(this)
         if (acct != null) {
             sign_in_button.visibility = View.GONE
-            /*tv_name.text = acct.displayName
-            tv_name.visibility = View.VISIBLE*/
+
+
+          //  signOut.isClickable = true
+          //  signOut.visibility = View.VISIBLE
+
+
+
             val intent = Intent(application, Participantes::class.java)
             startActivity(intent)
+            finish();
         }
+        /*signOut.setOnClickListener { view: View? ->  mGoogleSignInClient.signOut().addOnCompleteListener { task: Task<Void> -> if (task.isSuccessful) {
+
+            sign_in_button.visibility = View.VISIBLE
+            signOut.visibility = View.GONE
+
+            sign_in_button.isClickable = true
+            finish();
+        }
+        }
+        }*/
     }
+
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -62,6 +144,9 @@ class MainActivity : AppCompatActivity() {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             handleSignInResult(task)
         }
+        else
+            mCallbackManager!!.onActivityResult(requestCode, resultCode, data)
+
     }
 
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
@@ -70,16 +155,21 @@ class MainActivity : AppCompatActivity() {
 
             // Signed in successfully, show authenticated UI.
             sign_in_button.visibility = View.GONE
+            //sign_out_button.visibility= View.VISIBLE
+           // sign_out_button.isClickable=true
+
+
             /*tv_name.text = account!!.displayName
             tv_name.visibility = View.VISIBLE*/
             val intent = Intent(application, Participantes::class.java)
             startActivity(intent)
+            finish();
         } catch (e: ApiException) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
             sign_in_button.visibility = View.VISIBLE
-            tv_name.text = ""
-            tv_name.visibility = View.GONE
+            result_name.text = ""
+            result_name.visibility = View.GONE
         }
     }
 }
--- ConexionApiRestAndroidProject/app/src/main/java/com/example/logindemo/Participantes.kt
+++ ConexionApiRestAndroidProject/app/src/main/java/com/example/logindemo/Participantes.kt
@@ -1,26 +1,34 @@
 package com.example.logindemo
 
-import android.app.DownloadManager
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.AuthFailureError
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.VolleyError
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.facebook.*
+import com.facebook.AccessToken
+import com.facebook.login.LoginManager
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_participantes.*
 import org.json.JSONException
 import org.json.JSONObject
-import kotlin.jvm.Throws
+
 
 class Participantes : AppCompatActivity() {
 
     lateinit var agregarPersonal : Button
+    lateinit var logout_facebook_btnn : Button
     lateinit var listarPersonal : Button
     lateinit var txt_nombre : EditText
     lateinit var txt_cedula : EditText
@@ -28,21 +36,15 @@ class Participantes : AppCompatActivity() {
     lateinit var txt_correo : EditText
     lateinit var txt_observaciones : EditText
     //lateinit var prefHelper: PrefHelper
-
+    private var mCallbackManager: CallbackManager? = null
     private val URL_ROOT = "https://apiregistro.000webhostapp.com/API_PERSONAL/v1/?opt="
     val URL_ADD_PERSONAL = URL_ROOT + "addpersonal"
     val URL_LISTAR_PERSONAL = URL_ROOT + "getpersonal"
-
+    private val TAG = "FacebookLogin"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_participantes)
-
-        //prefHelper = PrefHelper(this)
-
-        /* buttonLogout.setOnClickListener {
-            //prefHelper.clear()
-            moveIntent()
-        }*/
+        mCallbackManager = CallbackManager.Factory.create()
 
         agregarPersonal = findViewById(R.id.btn_agregar)
         listarPersonal = findViewById(R.id.btn_listar)
@@ -52,14 +54,80 @@ class Participantes : AppCompatActivity() {
         txt_correo = findViewById(R.id.txt_correo)
         txt_observaciones = findViewById(R.id.txt_obs)
 
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.client_googleid))
+                .requestEmail()
+                .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        var intent=getIntent()
+        val tokefb = intent.getStringExtra("tokefb")
+
+
+        val acct = GoogleSignIn.getLastSignedInAccount(this)
+
+
+
+        if(tokefb!=null)
+        {
+           /* Toast.makeText(applicationContext, tokefb, Toast.LENGTH_LONG)
+                .show()*/
+            logout_facebook.visibility= View.VISIBLE
+
+        }
+        if (acct != null) {
+
+            sign_out_google.visibility= View.VISIBLE
+
+        }
+
+
+
+
+
+        sign_out_google.setOnClickListener { view: View? ->  mGoogleSignInClient.signOut().addOnCompleteListener { task: Task<Void> -> if (task.isSuccessful) {
+
+            /*Toast.makeText(applicationContext, "Cerrado", Toast.LENGTH_LONG)
+                    .show()*/
+            val intent = Intent(application, MainActivity::class.java)
+            startActivity(intent)
+            sign_out_google.visibility= View.GONE
+
+            finish();
+                }
+                }
+                }
+
+
+
         agregarPersonal.setOnClickListener{
             addPersonal()
+        }
+
+        logout_facebook.setOnClickListener{
+            logoutFace()
         }
 
         listarPersonal.setOnClickListener{
             val intent = Intent(application, ListaParticipantes::class.java)
             startActivity(intent)
         }
+
+
+    }
+
+
+    private fun logoutFace() {
+        // Logout from Facebook
+
+            LoginManager.getInstance().logOut()
+             val intent = Intent(application, MainActivity::class.java)
+            startActivity(intent)
+        finish();
+        logout_facebook.visibility= View.GONE
+
+
     }
 
     private fun moveIntent(){
